bd3ec373c1bd9dc39ce19afec7d3e18dadfc4209
mflix24 <banglacourse1223@gmail.com>
2026-04-22 11:25:35 +0100

    Project structure created

---
+++ template.sh
@@ -0,0 +1,14 @@
+# Creating Directories
+mkdir -p backend
+mkdir -p models
+mkdir -p data
+mkdir -p notebooks
+mkdir -p frontend
+
+
+# Creating Files
+# touch src/__init__.py
+# touch .env
+
+
+echo "Directory and Files Created Successfully"
